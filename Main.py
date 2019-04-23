162890222c8aa181ac6a7e5ce1333eb9e548994d
pawel717 <psuchanicz@gmail.com>
2019-04-24 00:45:36 +0200

    f1-score added

--- Main.py
+++ Main.py
@@ -10,6 +10,7 @@ from LDA import *
 from PCA import *
 from Normalization import Normalizer
 import Plot
+from sklearn.metrics import f1_score
 
 
 def main():
@@ -41,17 +42,17 @@ def main():
 
     # transform data with PCA and plot results
     dataTransformedPCA = transformPCA(normalizer.standarizeStandard(ourData))
-    Plot.scatterPlotNSP(dataTransformedPCA, ourData['NSP'])
-    Plot.scatterPlotCLASS(dataTransformedPCA, ourData['CLASS'])
+    #Plot.scatterPlotNSP(dataTransformedPCA, ourData['NSP'])
+    #Plot.scatterPlotCLASS(dataTransformedPCA, ourData['CLASS'])
 
     # transform data with LDA and plot results
-    dataTransformedLDA = transformLDA(ourData, 'NSP')
-    Plot.scatterPlotNSP(dataTransformedLDA, ourData['NSP'])
-    dataTransformedLDA = transformLDA(ourData, 'CLASS')
-    Plot.scatterPlotCLASS(dataTransformedLDA, ourData['CLASS'])
+    #ourData = transformLDA(ourData, 'NSP')
+    #Plot.scatterPlotNSP(dataTransformedLDA, ourData['NSP'])
+    #dataTransformedLDA = transformLDA(ourData, 'CLASS')
+    #Plot.scatterPlotCLASS(dataTransformedLDA, ourData['CLASS'])
 
     # plot heatmap - correlation between data and
-    Plot.heatmap(ourDataNormalized)
+    #Plot.heatmap(ourDataNormalized)
 
 
     ourDataNormalized = ourDataNormalized.drop(labels=['CLASS','NSP'],axis=1)
@@ -61,24 +62,41 @@ def main():
     #hoc.CreateHistAfterValidationCLASS(crossValidationDataCLASS, ourData['CLASS'])
 
 
-    #y_pred_nps = mysvm.svmClassificationSimple(ourData,classification_columns,'NSP')
-    #y_pred_class = mysvm.svmClassificationSimple(ourData,classification_columns,'CLASS')
-    #mysvm.plot_confusion_matrix(y_true=classification_columns['NSP'].values, y_pred=y_pred_nps,classes=nameOfNSP)
-    #mysvm.plot_confusion_matrix(y_true=classification_columns['CLASS'].values, y_pred=y_pred_class,classes=nameOfCLASS)
+    y_pred_nps = mysvm.svmClassificationSimple(ourData,classification_columns,'NSP')
+    mysvm.plot_confusion_matrix(y_true=classification_columns['NSP'].values, y_pred=y_pred_nps,classes=nameOfNSP, normalize=True)
     score_nps = mysvm.svmClassificationSimpleScore(ourData,classification_columns, 'NSP')
-    print(str(score_nps*100) + "% skutecznosci svm")
+    print(str(score_nps*100) + "% skutecznosci svm nsp")
+    print("f1-score svm nsp")
+    print(f1_score(classification_columns['NSP'].values, y_pred_nps, average=None))
+
+    y_pred_class = mysvm.svmClassificationSimple(ourData,classification_columns,'CLASS')
+    mysvm.plot_confusion_matrix(y_true=classification_columns['CLASS'].values, y_pred=y_pred_class,classes=nameOfCLASS, normalize=True)
+    score_class = mysvm.svmClassificationSimpleScore(ourData,classification_columns, 'CLASS')
+    print(str(score_class*100) + "% skutecznosci svm class")
+    print("f1-score svm class")
+    print(f1_score(classification_columns['CLASS'].values, y_pred_class, average=None))
+
     # y_pred_nps = mysvm.svmClassificationSimple(ourDataNormalized, classification_columns, 'NSP')
     # y_pred_class = mysvm.svmClassificationSimple(ourDataNormalized, classification_columns, 'CLASS')
     # mysvm.plot_confusion_matrix(y_true=classification_columns['NSP'].values, y_pred=y_pred_nps, classes=nameOfNSP)
     # mysvm.plot_confusion_matrix(y_true=classification_columns['CLASS'].values, y_pred=y_pred_class, classes=nameOfCLASS)
 
+
     y_pred_knn_nsp = knn.kNNClass(ourData,classification_columns,'NSP')
     mysvm.plot_confusion_matrix(y_true=classification_columns['NSP'].values, y_pred=y_pred_knn_nsp,classes=nameOfNSP, normalize=True)
     plt.show()
     score_knn_nps = knn.kNNClassScore(ourData,classification_columns, 'NSP')
-    print(str(score_knn_nps*100) + "% skutecznosci knn")
+    print(str(score_knn_nps*100) + "% skutecznosci knn nsp")
+    print("f1-score knn nsp")
+    print(f1_score(classification_columns['NSP'].values, y_pred_knn_nsp, average=None))
 
-
+    y_pred_knn_class = knn.kNNClass(ourData,classification_columns,'CLASS')
+    mysvm.plot_confusion_matrix(y_true=classification_columns['CLASS'].values, y_pred=y_pred_knn_class,classes=nameOfCLASS, normalize=True)
+    plt.show()
+    score_knn_class = knn.kNNClassScore(ourData,classification_columns, 'CLASS')
+    print(str(score_knn_class*100) + "% skutecznosci knn class")
+    print("f1-score knn class")
+    print(f1_score(classification_columns['CLASS'].values, y_pred_knn_class, average=None))
 
 #wywolanie funkcji main
 if __name__ == '__main__':
